3668fcfd66e4ce63b7448edb6f9c26da7df37622
Ajaspj <ajaspj123@gmail.com>
2024-04-08 00:36:25 +0530

    11

--- lib/view/homescreen/homescreen.dart
+++ lib/view/homescreen/homescreen.dart
@@ -10,58 +10,69 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   @override
+  void initState() {
+    fetchDAta();
+    super.initState();
+  }
+
+  fetchDAta() async {
+    await HomeScreencontroller.getAllData();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              ElevatedButton(
-                onPressed: () async {
-                  await HomeScreencontroller.getAllData();
-                  setState(() {});
-                },
-                child: const Text('get data'),
-              ),
-              ...List.generate(
-                HomeScreencontroller.studentlist.length,
-                (index) => ListTile(
-                  title: Text(HomeScreencontroller.studentlist[index].name),
-                  subtitle: Text(
-                      HomeScreencontroller.studentlist[index].ph.toString()),
-                  trailing: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      IconButton(
-                        onPressed: () async {
-                          await HomeScreencontroller.editData(
-                              HomeScreencontroller.studentlist[index].id);
-                          setState(() {});
-                        },
-                        icon: const Icon(Icons.edit),
-                      ),
-                      IconButton(
-                        onPressed: () async {
-                          await HomeScreencontroller.deleteData(
-                              HomeScreencontroller.studentlist[index].id);
-                          setState(() {});
-                        },
-                        icon: const Icon(Icons.delete),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
           await HomeScreencontroller.addData();
           setState(() {});
         },
+      ),
+      appBar: AppBar(),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            ElevatedButton(
+                onPressed: () async {
+                  await HomeScreencontroller.getAllData();
+                  setState(() {});
+                },
+                child: Text("get")),
+            Column(
+              children: List.generate(
+                  HomeScreencontroller.data.length,
+                  (index) => ListTile(
+                        title: Text(HomeScreencontroller.studentlist[index].name
+                            .toString()),
+                        subtitle: Text(HomeScreencontroller
+                            .studentlist[index].ph
+                            .toString()),
+                        trailing: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            IconButton(
+                                onPressed: () async {
+                                  await HomeScreencontroller.deleteData(
+                                      HomeScreencontroller
+                                          .studentlist[index].id);
+                                  setState(() {});
+                                },
+                                icon: Icon(Icons.delete)),
+                            IconButton(
+                                onPressed: () async {
+                                  await HomeScreencontroller.editData(
+                                      HomeScreencontroller
+                                          .studentlist[index].id);
+                                  setState(() {});
+                                },
+                                icon: Icon(Icons.edit)),
+                          ],
+                        ),
+                      )),
+            )
+          ],
+        ),
       ),
     );
   }
